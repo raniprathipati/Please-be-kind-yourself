78a3f7fb36c1c1daa75a07cd2e2642a85f1fadef
DELL <DELL@RANIRAJESH.P>
2024-07-04 20:45:00 +0530

    No one can take care of you

---
+++ src/kalyanSir/BeHelpfulPerson.java
@@ -0,0 +1,12 @@
+package kalyanSir;
+
+public class BeHelpfulPerson {
+
+	public static void main(String[] args) {
+	System.out.println("Our Main Agenda is to teach core"
+			+ " functionality to students like as primary colors");
+	System.out.println("Arif is a Good person");
+
+	}
+
+}
