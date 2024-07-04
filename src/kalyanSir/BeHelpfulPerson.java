b5a2399f66811569b83f91a96a572cf625eb9de3
Rani <165452264+raniprathipati@users.noreply.github.com>
2024-07-04 20:58:01 +0530

    Update BeHelpfulPerson.java

--- src/kalyanSir/BeHelpfulPerson.java
+++ src/kalyanSir/BeHelpfulPerson.java
@@ -6,7 +6,8 @@ public class BeHelpfulPerson {
 	System.out.println("Our Main Agenda is to teach core"
 			+ " functionality to students like as primary colors");
 	System.out.println("Arif is a Good person");
-
+	System.out.println("Arif is a kind person");
+	System.out.println("Arif is a wise person");
 	}
 
 }
